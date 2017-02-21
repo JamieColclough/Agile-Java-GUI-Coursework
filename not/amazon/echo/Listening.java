9b48bea265097e9745984672929f22ff7fb69596
jc781 <jc781@exeter.ac.uk>
2017-02-21 12:57:55 +0000

    Uploaded Implementation of listening state

---
+++ not/amazon/echo/Listening.java
@@ -0,0 +1,33 @@
+package notAmazonEcho;
+
+/**
+ * State representing the listening phase of the product
+ * waiting until sound is detected
+ * @author James Colclough
+ * @version 1.1
+ */
+public class Listening implements State{
+    @Override
+    public String toString(){
+        return "Listening";
+    }
+    
+
+    @Override
+    public void doAction(StateMachine stateMachine){
+        //Code involving listening for speech
+        
+        stateMachine.setState(new Responding());
+    }
+    
+    /**
+     * Additional Method in case the product is turned off before doAction() is called
+     * 
+     * @param stateMachine
+     */
+    public void turnOff(StateMachine stateMachine){
+        //Code involving turning off- i.e. turn off sound, light change
+        
+        stateMachine.setState(new OnOff());
+    }
+}
