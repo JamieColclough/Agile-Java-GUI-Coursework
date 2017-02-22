ebec79284e835a81af29e6db8da018432edd8f3c
Adam Mitchell <adammitchell@Adams-MacBook-Pro.local>
2017-02-22 16:51:28 +0000

    Fixed one background image not having the correct file path

--- src/not/amazon/echo/gui/EchoInterface.java
+++ src/not/amazon/echo/gui/EchoInterface.java
@@ -14,7 +14,7 @@ import javax.swing.*;
 
 public class EchoInterface extends JFrame {
     public final ImageIcon iconEcho = new ImageIcon("res/echo.jpg");
-    public final ImageIcon iconEchoAnswer = new ImageIcon("iconEchoAnswer.jpg");
+    public final ImageIcon iconEchoAnswer = new ImageIcon("res/echoAnswer.jpg");
     public final ImageIcon iconEchoOff = new ImageIcon("res/echoOff.jpg");
     final OnButton btn1 = new OnButton();
     private JLabel label;
